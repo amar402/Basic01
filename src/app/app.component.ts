3e52f58909e557f42b247d16c1e31c880157fbf0
amar402 <32408253+amar402@users.noreply.github.com>
2018-08-02 19:24:55 +0530

    Aug 2nd office signoff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = ' BASIC app ';
-  version:number = 6;
-  dataInput:any="";
+  version:number = 6.0 ;
+  person ={'Langauages':[]};
+  gender;
+  eng;fr;frs;
+  checkedVal= [] ;
   submitted(){
     alert(this.title +" with version number "+ this.version +" has submitted data")
   };
+  onSelection(val){
+    this.gender = val
+  }
+  onCheck(event:any){
+    
+    if(event.currentTarget.checked){
+      this.checkedVal.push(event.currentTarget.name)     
+    }else if(this.checkedVal.indexOf(event.currentTarget.name)>-1){
+      let i = this.checkedVal.indexOf(event.currentTarget.name)
+      this.checkedVal.splice(i,1)     
+    }
+    this.person.Langauages = this.checkedVal;
+  }
 }
